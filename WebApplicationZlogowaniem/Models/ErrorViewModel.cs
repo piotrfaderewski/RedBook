5906be37e0685a3ed49c23d9a8cb885d5504934a
Papcio Chmiel <pfaderewski@gmail.com>
2019-12-02 14:38:08 +0100

    initial setup

---
+++ WebApplicationZlogowaniem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationZlogowaniem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
