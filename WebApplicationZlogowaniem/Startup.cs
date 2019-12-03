e5d27c987a34f6e7a9b6b88aef704e6ea4d05502
Papcio Chmiel <pfaderewski@gmail.com>
2019-12-03 14:15:26 +0100

    EntityFrameworkIdentity CodeFirst approach

--- WebApplicationZlogowaniem/Startup.cs
+++ WebApplicationZlogowaniem/Startup.cs
@@ -29,8 +29,8 @@ namespace ElectronicRedBook
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(
+                    Configuration.GetConnectionString("MyConection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
