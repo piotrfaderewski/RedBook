2c56f305ceaca4ff8c75d2fdedee8a9e2ce91a45
mwagrodzki <m.wagrodzki@gmail.com>
2019-12-16 13:54:03 +0100

    addend edit/save/cancel buttons for each "form"

--- WebApplicationZlogowaniem/wwwroot/js/site.js
+++ WebApplicationZlogowaniem/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggle() {
+    var target_group = this.id.slice(0, -1);
+    var btn1 = document.getElementById(target_group + '1');
+    var btn2 = document.getElementById(target_group + '2');
+
+    if (this.id === btn1.id) {
+        this.style.display = 'none';
+        btn2.innerText = 'Edit';
+    } else if (this.id === btn2.id) {
+        if (this.innerText === 'Edit') {
+            btn1.style.display = 'inline';
+            btn2.innerText = 'Cancel';
+        } else {
+            btn1.style.display = 'none';
+            btn2.innerText = 'Edit';
+        }
+    }
+}
+
+document.getElementById('first_btn1').onclick = toggle;
+document.getElementById('first_btn2').onclick = toggle;
+
+document.getElementById('second_btn1').onclick = toggle;
+document.getElementById('second_btn2').onclick = toggle;
+
+document.getElementById('third_btn1').onclick = toggle;
+document.getElementById('third_btn2').onclick = toggle;
