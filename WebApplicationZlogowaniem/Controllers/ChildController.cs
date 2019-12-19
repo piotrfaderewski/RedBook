8121c2a5118ba13651e61ff4326e4e089de4757a
mwagrodzki <m.wagrodzki@gmail.com>
2019-12-19 10:55:57 +0100

    Labor page added to Child subpages

--- WebApplicationZlogowaniem/Controllers/ChildController.cs
+++ WebApplicationZlogowaniem/Controllers/ChildController.cs
@@ -42,6 +42,11 @@ namespace WebApplicationZlogowaniem.Controllers
             return View();
         }
 
+        public IActionResult Labor()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
